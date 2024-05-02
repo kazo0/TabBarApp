63e45611f7e4cb78f4e89e1c372d57cc93af282e
Steve Bilogan <steve.bilogan@gmail.com>
2024-05-02 13:54:57 -0400

    finish

--- TabBarApp/MainPage.xaml.cs
+++ TabBarApp/MainPage.xaml.cs
@@ -6,4 +6,20 @@ public sealed partial class MainPage : Page
     {
         this.InitializeComponent();
     }
+
+	private void TopTabSelectionChanged(TabBar sender, TabBarSelectionChangedEventArgs args)
+	{
+		if (args.NewItem is TabBarItem item) 
+		{
+			TopTab.Text = item.Content as string;
+		}
+    }
+
+	private void BottomTabSelectionChanged(TabBar sender, TabBarSelectionChangedEventArgs args)
+	{
+		if (args.NewItem is TabBarItem item)
+		{
+			BottomTab.Text = item.Content as string;
+		}
+	}
 }
